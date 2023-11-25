e30d91f823c6177c8c95f9f59379403e0c1b3510
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-11-25 13:15:30 +0300

    Add files via upload

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-int	ft_atoi(const char *nptr)
+int	ft_atoi(const char *str)
 {
 	int	i;
 	int	sign;
@@ -21,20 +21,20 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	sign = 1;
 	container = 0;
-	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (nptr[i] == '-')
+		if (str[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	while (nptr[i])
+	while (str[i])
 	{
-		if (!(ft_isdigit(nptr[i])))
+		if (!(ft_isdigit(str[i])))
 			return (container * sign);
 		else
-			container = container * 10 + (const char ) nptr[i] - 48;
+			container = container * 10 + (const char ) str[i] - 48;
 		i++;
 	}
 	return (container * sign);
